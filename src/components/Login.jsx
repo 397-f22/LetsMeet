486f572be438f1060301798c7c1eb0e91f620448
Yousef Farge <yousef.farge@gmail.com>
2022-10-08 15:54:34 -0500

    created login form step

---
+++ src/components/Login.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const Login = ({ handleChange }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "15rem",
+        alignItems: "center",
+      }}
+    >
+      <h1
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          padding: "10px",
+        }}
+      >
+        Robotics Club Meeting
+      </h1>
+      <div>
+        <label style={{ display: "block" }}>Name:</label>
+        <input type="text" onChange={handleChange} />
+      </div>
+      <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+        <button
+          style={{
+            borderRadius: "4px",
+            padding: "5px 40px",
+            backgroundColor: "#414BB2",
+            color: "white",
+          }}
+        >
+          Find Times
+        </button>
+        <button
+          style={{
+            borderRadius: "4px",
+            padding: "5px 40px",
+            backgroundColor: "#414BB2",
+            color: "white",
+          }}
+        >
+          Select Times
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,37 +1,40 @@
-import React, { Component } from 'react'
-import Date from './Date'
-import FormStepper from './FormStepper';
+import React, { Component } from "react";
+import Date from "./Date";
+import FormStepper from "./FormStepper";
+import Login from "./Login";
 
 export default class Signup extends Component {
-
   state = {
-    step: 1,
-    username: '', 
-  }
+    step: 0,
+    username: "",
+  };
 
   // go back to previous step
   prevStep = () => {
     const { step } = this.state;
     this.setState({ step: step - 1 });
-  }
+  };
 
   // proceed to the next step
   nextStep = () => {
     const { step } = this.state;
     this.setState({ step: step + 1 });
-  }
+  };
 
   // Handle fields change
-  handleChange = input => e => {
+  handleChange = (input) => (e) => {
     this.setState({ [input]: e.target.value });
-  }
+  };
 
   render() {
     const { step } = this.state;
     const { username } = this.state;
-    const values = { username }
-    
-    return <FormStepper/>
+    const values = { username };
+
+    return (
+      <FormStepper step={this.state.step}>
+        <Login handleChange={this.handleChange} />
+      </FormStepper>
+    );
   }
 }
-
