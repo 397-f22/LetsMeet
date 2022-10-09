a65389b5d2ad8a003c2a4fe0fdb002fabe09fb33
Yousef Farge <yousef.farge@gmail.com>
2022-10-09 15:01:37 -0500

    added isSelectedDay; added isSelectedDays; added isMeetingTimeOkay;

--- src/utilities/filter.js
+++ src/utilities/filter.js
@@ -1,4 +1,9 @@
-export const filter = (data, requestedLength, requiredMembers) => {
+export const filter = (
+  data,
+  requestedLength,
+  requiredMembers,
+  requestedDays
+) => {
   // data: {
   //   <username1>: [0, 1, 2, 10, 11, 12, 13, 21, 22, 23], // buckets
   //   <username2>: [...],
@@ -15,30 +20,56 @@ export const filter = (data, requestedLength, requiredMembers) => {
 
   const numParticipants = Object.keys(data).length;
 
-  let buckets = Array(48*7).fill().map(() => Array(0));
+  let buckets = Array(48 * 7)
+    .fill()
+    .map(() => Array(0));
 
   Object.entries(data).map(([username, arr]) => {
     console.log(username);
-    console.log(arr); 
-    arr.forEach((v) => {   
+    console.log(arr);
+    arr.forEach((v) => {
       buckets[v].push(username);
     });
   });
 
-  let count2buckets = Array(numParticipants+1).fill().map(() => Array(0));
+  let count2buckets = Array(numParticipants + 1)
+    .fill()
+    .map(() => Array(0));
   buckets.forEach((arr, i) => {
     count2buckets[arr.length].append(i);
   });
 
   const slotHasMembers = (slot, members) => {
-    members.every((member) => buckets[slot].includes(member));  
-  }
+    if (slot >= buckets.length) return false;
+    return members.every((member) => buckets[slot].includes(member));
+  };
 
-  const timeSlotOkay = (startingSlot) => {
-    for (let slot = startingSlot; slot < requestedLength; slot++) {
-      
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const encodeDay = (day) => days.findIndex(day);
+
+  const isSelectedDay = (slot, day) => Math.floor(slot / 48) === encodeDay(day);
+
+  const isSelectedDays = (slot, days) =>
+    days.some((day) => isSelectedDay(slot, day));
+
+  const isMeetingTimeOkay = (startTime, members, days) => {
+    for (let slot = startTime; slot < startTime + requestedLength; slot++) {
+      if (!(slotHasMembers(slot, members) && isSelectedDays(slot, days)))
+        return false;
     }
-  }
+    return true;
+  };
 
-
+  return buckets.filter((members, startTime) =>
+    isMeetingTimeOkay(startTime, requiredMembers, requestedDays)
+  );
 };
