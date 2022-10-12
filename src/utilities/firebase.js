0a5baf5720bf6cd068c16100d0f473eac4ea75c3
Yousef Farge <yousef.farge@gmail.com>
2022-10-12 13:20:43 -0500

    added firebase app initialization

---
+++ src/utilities/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBspAveJg8sXsKNcMSeH9EuLC48vbaSmkk",
+  authDomain: "letsmeet-51304.firebaseapp.com",
+  databaseURL: "https://letsmeet-51304-default-rtdb.firebaseio.com",
+  projectId: "letsmeet-51304",
+  storageBucket: "letsmeet-51304.appspot.com",
+  messagingSenderId: "517774577396",
+  appId: "1:517774577396:web:61cfdead55e41ef75d18a1",
+};
+
+const app = initializeApp(firebaseConfig);
