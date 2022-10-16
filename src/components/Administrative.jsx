a16b3199d9b27a4dac74828311649f780ecf62b9
TH Huang <tzhsn.huang@gmail.com>
2022-10-16 13:16:21 -0500

    add start and end time input fields

--- src/components/Administrative.jsx
+++ src/components/Administrative.jsx
@@ -1,34 +1,48 @@
-import React, { useState } from "react";
-import Date from "./Date";
-import FormStepper from "./FormStepper";
-import Login from "./Login";
+import React, { useState } from 'react';
+import Date from './Date';
+import FormStepper from './FormStepper';
+import Login from './Login';
 
 const validateUserData = (key, val) => {
   switch (key) {
-    default: return '';
+    default:
+      return '';
   }
 };
 
-const InputField = ({name, text, state, change}) => (
+const InputField = ({ name, text, state, change }) => (
   <div className="mb-3">
-    <label htmlFor={name} className="form-label">{text}</label>
-    <input className="form-control" id={name} name={name} 
-      defaultValue={state.values?.[name]} onChange={change} />
+    <label htmlFor={name} className="form-label">
+      {text}
+    </label>
+    <input
+      className="form-control"
+      id={name}
+      name={name}
+      defaultValue={state.values?.[name]}
+      onChange={change}
+    />
     <div className="invalid-feedback">{state.errors?.[name]}</div>
   </div>
 );
 
-const ButtonBar = ({message, disabled}) => {
+const ButtonBar = ({ message, disabled }) => {
   return (
     <div className="d-flex">
-      <button type="submit" className="btn btn-primary me-auto" disabled={disabled}>Submit</button>
+      <button
+        type="submit"
+        className="btn btn-primary me-auto"
+        disabled={disabled}
+      >
+        Submit
+      </button>
       <span className="p-2">{message}</span>
     </div>
   );
 };
 
-const SignupEditor = ({ }) => {
-  const eventId = uuidv4(); 
+const SignupEditor = ({}) => {
+  const eventId = uuidv4();
   const [update, result] = useDbUpdate(`/event/${eventId}`);
   const [state, change] = useFormData(validateUserData);
   const submit = (evt) => {
@@ -39,42 +53,110 @@ const SignupEditor = ({ }) => {
   };
   return (
     <form onSubmit={submit} className={state.errors ? 'was-validated' : null}>
-      <InputField name="meetingName" text="Meeting Name" state={state} change={change}/>
-      <InputField name="description" text="Description" state={state} change={change}/>
-      <ButtonBar message=""/>
+      <InputField
+        name="meetingName"
+        text="Meeting Name"
+        state={state}
+        change={change}
+      />
+      <InputField
+        name="description"
+        text="Description"
+        state={state}
+        change={change}
+      />
+      <ButtonBar message="" />
     </form>
   );
-}
+};
 
 const Signup = () => {
   return (
     <div className="container">
       <h1>LetsMeet</h1>
-      <SignupEditor/>
+      <SignupEditor />
       <div>Days of a Week</div>
     </div>
   );
 };
 
 const Administrative = (handleChange, values, Signup) => {
+  const weekDays = ['M', 'Tu', 'W', 'TH', 'Fr', 'Sa', 'Su'];
+  const startHours = [
+    '00',
+    '01',
+    '02',
+    '03',
+    '04',
+    '05',
+    '06',
+    '07',
+    '08',
+    '09',
+    '10',
+    '11',
+    '12',
+    '13',
+    '14',
+    '15',
+    '16',
+    '17',
+    '18',
+    '19',
+    '20',
+    '21',
+    '22',
+    '23',
+  ];
+  const endHours = [
+    '01',
+    '02',
+    '03',
+    '04',
+    '05',
+    '06',
+    '07',
+    '08',
+    '09',
+    '10',
+    '11',
+    '12',
+    '13',
+    '14',
+    '15',
+    '16',
+    '17',
+    '18',
+    '19',
+    '20',
+    '21',
+    '22',
+    '23',
+    '24',
+  ];
 
-    const weekDays = ['M', 'Tu', 'W', 'TH', 'Fr', 'Sa', 'Su'];
+  const [meetingNameState, setMeetingNameState] = useState();
+  const [descriptionState, setDescriptionState] = useState();
+  const [daysState, setDaysState] = useState();
+  const [startState, setStartState] = useState();
+  const [endState, setEndState] = useState();
+
   return (
     <div
       className="container"
       style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-around",
-        height: "100vh",
-        alignItems: "center",
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        height: '100vh',
+        alignItems: 'center',
       }}
     >
       <h1
         style={{
-          display: "flex",
-          justifyContent: "center",
-          padding: "20px",
+          display: 'flex',
+          justifyContent: 'center',
+          padding: '20px',
         }}
       >
         Lets Meet
@@ -83,9 +165,9 @@ const Administrative = (handleChange, values, Signup) => {
         New Meeting
         <input
           style={{
-            display: "block",
-            borderRadius: "4px",
-            padding: "5px 10px",
+            display: 'block',
+            borderRadius: '4px',
+            padding: '5px 10px',
           }}
           type="text"
           placeholder="Meeting Name"
@@ -97,9 +179,9 @@ const Administrative = (handleChange, values, Signup) => {
         Description
         <input
           style={{
-            display: "block",
-            borderRadius: "4px",
-            padding: "5px 10px",
+            display: 'block',
+            borderRadius: '4px',
+            padding: '5px 10px',
           }}
           type="text"
           placeholder="Optional"
@@ -108,17 +190,51 @@ const Administrative = (handleChange, values, Signup) => {
         />
       </label>
 
-      <div className="daySelector" style={{display:"flex"}}>
-        {weekDays.map(day => (<div className="daySelectorTile" style={{width: "2.5rem", border: "1px solid #000000", textAlign: "center"}}>{day}</div>))}
+      <div className="daySelector" style={{ display: 'flex' }}>
+        {weekDays.map((day) => (
+          <div
+            className="daySelectorTile"
+            style={{
+              width: '2.5rem',
+              border: '1px solid #000000',
+              textAlign: 'center',
+            }}
+          >
+            {day}
+          </div>
+        ))}
       </div>
-      <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+
+      <div style={{ display: 'flex', gap: '3rem' }}>
+        <div>
+          Start
+          <div>
+            <select>
+              {startHours.map((h) => (
+                <option value={h}>{h} </option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <div>
+          End
+          <div>
+            <select>
+              {startHours.map((h) => (
+                <option value={h}>{h} </option>
+              ))}
+            </select>
+          </div>
+        </div>
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
         <button
           style={{
-            borderRadius: "4px",
-            padding: "5px 40px",
-            backgroundColor: "#576e93",
-            color: "white",
-            border: "none",
+            borderRadius: '4px',
+            padding: '5px 40px',
+            backgroundColor: '#576e93',
+            color: 'white',
+            border: 'none',
           }}
           className="shadow-md"
           onClick={Signup}
