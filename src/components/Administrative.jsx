d26050a0f0411d0b99516fd6554bb5910ae3a517
TH Huang <tzhsn.huang@gmail.com>
2022-10-17 14:13:25 -0500

    added some styling to administrative page

--- src/components/Administrative.jsx
+++ src/components/Administrative.jsx
@@ -1,73 +1,70 @@
-import React, { useState } from "react";
-import Date from "./Date";
-import FormStepper from "./FormStepper";
-import Login from "./Login";
-import { v4 as uuidv4 } from "uuid";
-import { useDbUpdate } from "../utilities/firebase";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useDbUpdate } from '../utilities/firebase';
+import { useNavigate } from 'react-router-dom';
 
-const Administrative = (handleChange, values, Signup) => {
-  const weekDays = ["M", "Tu", "W", "Th", "Fr", "Sa", "Su"];
+const Administrative = () => {
+  const weekDays = ['M', 'Tu', 'W', 'Th', 'Fr', 'Sa', 'Su'];
   const startHours = [
-    "00",
-    "01",
-    "02",
-    "03",
-    "04",
-    "05",
-    "06",
-    "07",
-    "08",
-    "09",
-    "10",
-    "11",
-    "12",
-    "13",
-    "14",
-    "15",
-    "16",
-    "17",
-    "18",
-    "19",
-    "20",
-    "21",
-    "22",
-    "23",
+    '00',
+    '01',
+    '02',
+    '03',
+    '04',
+    '05',
+    '06',
+    '07',
+    '08',
+    '09',
+    '10',
+    '11',
+    '12',
+    '13',
+    '14',
+    '15',
+    '16',
+    '17',
+    '18',
+    '19',
+    '20',
+    '21',
+    '22',
+    '23',
   ];
   const endHours = [
-    "01",
-    "02",
-    "03",
-    "04",
-    "05",
-    "06",
-    "07",
-    "08",
-    "09",
-    "10",
-    "11",
-    "12",
-    "13",
-    "14",
-    "15",
-    "16",
-    "17",
-    "18",
-    "19",
-    "20",
-    "21",
-    "22",
-    "23",
-    "24",
+    '01',
+    '02',
+    '03',
+    '04',
+    '05',
+    '06',
+    '07',
+    '08',
+    '09',
+    '10',
+    '11',
+    '12',
+    '13',
+    '14',
+    '15',
+    '16',
+    '17',
+    '18',
+    '19',
+    '20',
+    '21',
+    '22',
+    '23',
+    '24',
   ];
 
   const [meetingNameState, setMeetingNameState] = useState();
-  const [descriptionState, setDescriptionState] = useState("");
+  const [descriptionState, setDescriptionState] = useState('');
   const [daysState, setDaysState] = useState([]);
   const [daysError, setDaysError] = useState(false);
   const [startEndError, setStartEndError] = useState(false);
-  const [startState, setStartState] = useState("09");
-  const [endState, setEndState] = useState("10");
+  const [startState, setStartState] = useState('09');
+  const [endState, setEndState] = useState('10');
 
   const eventId = uuidv4();
   const [update, result] = useDbUpdate(`/events/${eventId}`);
@@ -119,35 +116,44 @@ const Administrative = (handleChange, values, Signup) => {
     <div
       className="container"
       style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-around",
-        height: "100vh",
-        alignItems: "center",
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        height: '100vh',
+        alignItems: 'center',
       }}
     >
       <h1
         style={{
-          display: "flex",
-          justifyContent: "center",
-          padding: "20px",
+          display: 'flex',
+          justifyContent: 'center',
+          padding: '20px',
         }}
       >
         Lets Meet
       </h1>
-      <form id="create-meeting" onSubmit={(e) => onSubmit(e)}>
+      <form
+        id="create-meeting"
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-evenly',
+          height: '100vh',
+          alignItems: 'center',
+        }}
+        onSubmit={(e) => onSubmit(e)}
+      >
         <label>
           New Meeting
           <input
             required={true}
             style={{
-              display: "block",
-              borderRadius: "4px",
-              padding: "5px 10px",
+              display: 'block',
+              borderRadius: '4px',
+              padding: '5px 10px',
             }}
             type="text"
             placeholder="Meeting Name"
-            value={values.username}
             onChange={(e) => {
               setMeetingNameState(e.currentTarget.value);
             }}
@@ -157,36 +163,42 @@ const Administrative = (handleChange, values, Signup) => {
           Description
           <input
             style={{
-              display: "block",
-              borderRadius: "4px",
-              padding: "5px 10px",
+              display: 'block',
+              borderRadius: '4px',
+              padding: '5px 10px',
             }}
             type="text"
             placeholder="Optional"
-            value={values.username}
             onChange={(e) => {
               setDescriptionState(e.currentTarget.value);
             }}
           />
         </label>
-        <div className="daySelector" style={{ display: "flex" }}>
-          {weekDays.map((day) => (
-            <div
-              key={day}
-              className="daySelectorTile"
-              style={{
-                width: "2.5rem",
-                border: "1px solid #000000",
-                textAlign: "center",
-              }}
-              onClick={() => toggleSelected({ day })}
-            >
-              {day}
-            </div>
-          ))}
+
+        <div className="daySelector" style={{ display: 'flex' }}>
+          {weekDays.map((day, i) => {
+            let style = {
+              width: '2.5rem',
+              border: '1px solid black',
+              textAlign: 'center',
+            };
+            if (daysState.includes(day)) {
+              style = { ...style, backgroundColor: '#576e93', color: 'white' };
+            }
+            return (
+              <div
+                key={day}
+                className="daySelectorTile"
+                style={i === 0 ? style : { ...style, borderLeft: '0' }}
+                onClick={() => toggleSelected({ day })}
+              >
+                {day}
+              </div>
+            );
+          })}
         </div>
         {daysError && <p>You must select at least one day</p>}
-        <div style={{ display: "flex", gap: "3rem" }}>
+        <div style={{ display: 'flex', gap: '3rem' }}>
           <div>
             <label>
               Start
@@ -200,7 +212,7 @@ const Administrative = (handleChange, values, Signup) => {
               >
                 {startHours.map((h, id) => (
                   <option key={id} value={h}>
-                    {h}{" "}
+                    {h}{' '}
                   </option>
                 ))}
               </select>
@@ -218,7 +230,7 @@ const Administrative = (handleChange, values, Signup) => {
               >
                 {endHours.map((h, id) => (
                   <option key={id} value={h}>
-                    {h}{" "}
+                    {h}{' '}
                   </option>
                 ))}
               </select>
@@ -228,15 +240,15 @@ const Administrative = (handleChange, values, Signup) => {
         {startEndError && <p>Start time cannot be greater than end time</p>}
       </form>
 
-      <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+      <div style={{ marginBottom: '5rem' }}>
         <button
           form="create-meeting"
           style={{
-            borderRadius: "4px",
-            padding: "5px 40px",
-            backgroundColor: "#576e93",
-            color: "white",
-            border: "none",
+            borderRadius: '4px',
+            padding: '5px 40px',
+            backgroundColor: '#576e93',
+            color: 'white',
+            border: 'none',
           }}
           className="shadow-md"
         >
