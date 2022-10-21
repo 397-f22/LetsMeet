966c3137dfb91f8ffce2002821ef75900e554290
Yousef Farge <yousef.farge@gmail.com>
2022-10-21 13:42:56 -0500

    added dynamic name + description to Login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,39 +1,48 @@
-import React from 'react';
-import { useEffect } from "react";
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import { useRtdbData } from "../utilities/firebase";
 
 const Login = ({ handleChange, values, nextStep, eventId }) => {
-  const [event, loading] = useRtdbData(`events/${eventId}`);
-  console.log(event); 
   const navigate = useNavigate();
-  if (loading) return <div>loading...</div>;
+  const [data, isLoading] = useRtdbData(`events/${eventId}`);
+  if (isLoading) return <h1>Loading...</h1>;
+  const { name, description } = data;
+
   return (
     <div
       style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-around',
-        height: '100vh',
-        alignItems: 'center',
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-around",
+        height: "100vh",
+        alignItems: "center",
       }}
     >
-      <h1
+      <div
         style={{
-          display: 'flex',
-          justifyContent: 'center',
-          padding: '20px',
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
       >
-        {event.description}
-      </h1>
+        <h1
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            padding: "20px",
+          }}
+        >
+          {name}
+        </h1>
+        <p>{description}</p>
+      </div>
       <label>
         Name
         <input
           style={{
-            display: 'block',
-            borderRadius: '4px',
-            padding: '5px 10px',
+            display: "block",
+            borderRadius: "4px",
+            padding: "5px 10px",
           }}
           type="text"
           placeholder="Enter username here"
@@ -41,14 +50,14 @@ const Login = ({ handleChange, values, nextStep, eventId }) => {
           onChange={(e) => handleChange(e.currentTarget.value)}
         />
       </label>
-      <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+      <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
         <button
           style={{
-            borderRadius: '4px',
-            padding: '5px 40px',
-            backgroundColor: '#576e93',
-            color: 'white',
-            border: 'none',
+            borderRadius: "4px",
+            padding: "5px 40px",
+            backgroundColor: "#576e93",
+            color: "white",
+            border: "none",
           }}
           className="shadow-md"
           onClick={() => navigate(`filter`)}
@@ -57,11 +66,11 @@ const Login = ({ handleChange, values, nextStep, eventId }) => {
         </button>
         <button
           style={{
-            borderRadius: '4px',
-            padding: '5px 40px',
-            backgroundColor: '#576e93',
-            color: 'white',
-            border: 'none',
+            borderRadius: "4px",
+            padding: "5px 40px",
+            backgroundColor: "#576e93",
+            color: "white",
+            border: "none",
           }}
           className="shadow-md"
           onClick={nextStep}
